af9a626e526e866ec32829067547797be965906c
yui.ono <yui.a.ono@rakuten.com>
2018-06-22 00:50:49 +0000

    h

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -6,7 +6,7 @@
 
     <div class="row">
         <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
-            {!! Form::model($task, ['route' => 'tasks.store']) !!}
+            {!! Form::model($task, ['route' => ['tasks.update',$task->id], 'method' => 'put']) !!}
             
                 <div class="form-group">
                     {!! Form::label('status', 'ステイタス:') !!}
